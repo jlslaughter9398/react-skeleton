961d7e8401b0bf615224e3c6d9cd0ddc4a571ba7
jlslaughter9398 <james.slaughter08@gmail.com>
2016-03-14 15:01:48 -0500

    Extend first exercise to an actual simple React project

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,23 @@
 var React = require('react');
 var ListItem = require('./ListItem.jsx');
 
-var ingredients = [{"id":1,"text":"ham"}, {"id":2,"text":"cheese"}, {"id":3,"text":"shrimp"}, {"id":4,"text":"pizza"}];
-
+//var ingredients = [{"id":1,"text":"ham"}, {"id":2,"text":"cheese"}, {"id":3,"text":"shrimp"}, {"id":4,"text":"pizza"}];
+/*
 var List = React.createClass({
     render: function() {
         var listItems = ingredients.map(function(item) {
             return <ListItem key={item.id} ingredient={item.text}/>;
         });
         return (<ul>{listItems}</ul>);
+    }
+});
+*/
+var List = React.createClass({
+    render: function() {
+        var createItem = function(text, index) {
+            return <ListItem key={index + text} text={text}/>;
+        };
+        return (<ul>{this.props.items.map(createItem)}</ul>);
     }
 });
 
